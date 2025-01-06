d496f221f08daf1a72cd180f54c21a3ae0cc7cdf
misnosugianto48 <misnosugianto48@gmail.com>
2025-01-06 09:57:02 +0700

    config: add setup husky prepare and commit conventional

---
+++ commitlint.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'test',
+        'chore',
+        'revert',
+        'ci',
+        'perf',
+        'env',
+        'sec',
+        'config',
+        'api',
+      ],
+    ],
+  },
+};
